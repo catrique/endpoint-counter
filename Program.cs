4c9e6860995b727221bea170a4879ce1fdb88759
CaledTarique <caled.98@hotmail.com>
2025-08-15 15:19:03 -0300

    Contador de endpoints API .NET

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+var assembly = Assembly.Load("Events.API"); // ← nome do assembly, geralmente igual ao nome do projeto
+
+var controllers = assembly.GetTypes()
+    .Where(type => typeof(ControllerBase).IsAssignableFrom(type) && !type.IsAbstract);
+
+int totalEndpoints = 0;
+
+foreach (var controller in controllers)
+{
+    var methods = controller.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly)
+        .Where(m => m.GetCustomAttributes().Any(attr =>
+            attr.GetType().Name.StartsWith("Http")));
+
+    Console.WriteLine($"📁 {controller.Name}: {methods.Count()} endpoints");
+
+    totalEndpoints += methods.Count();
+}
+
+Console.WriteLine($"\n🔢 Total de endpoints encontrados: {totalEndpoints}");
